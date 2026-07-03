505fb20514b86a5d68cfe63243821430bc9abb6e
Keene <keene.ng.2024@smu.edu.sg>
2026-07-03 12:10:01 +0800

    Add: search-2d-matrix - submission-11

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-11.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        //binary search in binary search
+
+        int left = 0;
+        int right = matrix.length * matrix[0].length -1 ;
+        int cols = matrix[0].length;
+
+        while(left <= right){
+            int middle = left + (right - left)/2;
+            
+            //now lets calculate the position
+            int row = Math.floorDiv(middle, cols);
+            int col = middle % cols;
+            
+            if(target == matrix[row][col]){
+                return true;
+            }
+            else if(target < matrix[row][col]){
+                right = middle -1;
+            }
+            else{
+                left = middle + 1;
+            }
+            
+        }
+
+
+        return false;
+    }
+}
