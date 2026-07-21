67b69996af4d615f359ede28d41c9bd4719fd489
Keene <keene.ng.2024@smu.edu.sg>
2026-07-22 02:35:24 +0800

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] answer = new int[n+1];
+        int total = 0;
+
+
+
+        for(int i = answer.length -1; i >= 0; i--){
+            System.out.println(i);
+            if (i == n){
+                answer[i] = 1;
+            
+            }
+            else if(i == n-1){
+                answer[i] = 1;
+            }
+            else{
+                answer[i] = answer[i+2] + answer[i+1];
+            }
+        }
+
+        for(int g : answer){
+            System.out.println(g);
+        }
+
+
+        return answer[0];
+    }
+}
